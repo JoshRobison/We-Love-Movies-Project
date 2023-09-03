52d0015ee2f096fd0a720b694d753c716a2991c1
Joshua Robison <118320419+JoshRobison@users.noreply.github.com>
2023-09-03 14:25:46 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,8 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
